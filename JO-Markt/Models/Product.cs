495950fc4fcbbb781f55e0973e214b755bc0e904
Joshua Otter <joshuaotter@gmail.com>
2019-11-20 13:14:25 +0100

    fixed

--- JO-Markt/Models/Product.cs
+++ JO-Markt/Models/Product.cs
@@ -27,62 +27,6 @@ namespace JOMarkt.Models
         public string Subcategory { get; set; }
         [NotMapped]
         public object SubsubCategory { get; private set; }
-        public void EmptyTable(ApplicationDbContext _context, string Tablename)
-        {
-            _context.Database.ExecuteSqlCommand("TRUNCATE TABLE [" + Tablename + "]");
-        }
-
-        public void UpdateProducts(ApplicationDbContext _context)
-        {
-            bool ErrorOccured = false;
-            List<Product> products = new List<Product>();
-            try
-            {
-                string xmlurl = "https://supermaco.starwave.nl/api/products";
-                var doc = XDocument.Load(xmlurl);
-                Console.WriteLine(doc.Elements());
-                List<Product> itemlist = doc.Root
-                    .Descendants("Product")
-                    .Select(node => new Product
-                    {
-                        Id = int.Parse(node.Attribute("Id").Value),
-                        Title = node.Element("Title").Value,
-                        EAN = node.Element("EAN").Value,
-                        Brand = node.Element("Brand").Value,
-                        Shortdescription = node.Element("Shortdescription").Value,
-                        Fulldescription = node.Element("Fulldescription").Value,
-                        Image = node.Element("Image").Value,
-                        Weight = node.Element("Weight").Value,
-                        Price = double.Parse(node.Element("Price").Value),
-                        Category = node.Element("Category").Value,
-                        Subcategory = node.Element("Subcategory").Value,
-                        SubsubCategory = node.Element("Subsubcategory").Value
-                    })
-                    .ToList();
-                foreach (var item in itemlist)
-                {
-                    Console.WriteLine("Id : " + item.EAN);
-                }
-
-                _context.Product.AddRange(itemlist);
-                EmptyTable(_context, "Product");
-                _context.SaveChanges();
-            }
-            catch (Exception error)
-            {
-                Console.WriteLine("Error : " + error.Message);
-                ErrorOccured = true;
-            }
-            if (ErrorOccured == true)
-            {
-                Product product1 = new Product();
-                products.Add(product1);
-            }
-            else
-            {
-                return;
-            }
-
-        }
+      
     }
 }
