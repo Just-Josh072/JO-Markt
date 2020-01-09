8394d77229c7d37b7605eca6fd3f81c7999b85cd
Joshua Otter <joshuaotter@gmail.com>
2020-01-09 11:37:08 +0100

    register is mooier en beter

--- JO-Markt/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ JO-Markt/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -65,6 +65,7 @@ namespace JOMarkt.Areas.Identity.Pages.Account
             [Required]
             [DataType(DataType.Text)]
             public string Postcode { get; set; }
+            public int PhoneNumber { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
