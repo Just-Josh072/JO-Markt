91e92daa9eb5f31cf79fac5ca3e19fc11b7047d1
joe barbar <jbarbar9991@gmail.com>
2020-01-23 20:10:12 +0100

    webmanager

---
+++ JO-Markt/Areas/WebManager/Controllers/homeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using JOMarkt.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using JOMarkt.Models;
+using System.Collections;
+
+namespace JOMarkt.Areas.WebManager.Controllers
+{
+    public class homeController : Controller
+    {
+        ApplicationDbContext _context;
+        
+        RoleManager<IdentityRole> _roleManager;
+
+        public homeController(ApplicationDbContext context, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+        
+
+        }
+
+        [Area("WebManager")]
+
+        [Authorize(Roles = "WebManager")]
+
+       
+
+        public IActionResult Index()
+        {
+         
+         
+            return View();
+        }
+    }
+}
